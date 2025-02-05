0dd1e7d0bbc1ade8057d8f251601c6f1d8878950
ezeun <consistency1231@gmail.com>
2025-02-05 11:23:38 +0900

    js basic #finish

--- javascript-practices/basics/ex12.js
+++ javascript-practices/basics/ex12.js
@@ -1,5 +1,32 @@
 /* Lexical Scope */
+var f1 = function(){
+    var s = "hello world";
 
+    var inner = function(){
+        console.log(s);
+    }
+
+    inner();
+}
+
+f1();
 
 /* Closure is a function that has access to the outer function's scope in which it was created even after the outer function has finished executing. */
+var f1 = function(){
+    var s = "hello world";
 
+    var inner = function(){
+        console.log(s);
+    }
+
+    return inner;
+}
+
+var closure = f1();
+closure();
+
+function Component(){
+    var s = userState("10");
+
+    s = 20;
+}
